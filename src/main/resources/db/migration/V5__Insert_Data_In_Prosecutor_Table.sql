ba3469257c6b361b833c2c61018d7ddc5e7301a4
ALVARLEUNG <alvar.leung@qq.com>
2019-07-18 20:16:12 +0800

    [LST]feat:add the sql script V5

---
+++ src/main/resources/db/migration/V5__Insert_Data_In_Prosecutor_Table.sql
@@ -0,0 +1,5 @@
+SET SCHEMA court;
+INSERT INTO PROSECUTOR (id, name, sex, age, birthday, ID_number, experience) VALUES
+(1, '张三', 'male', 18, 2001/10/20, '001' , 0),
+(2, '李四', 'female', 28, 1982/10/20, '002', 10),
+(3, '王五', 'male', 58, 1972/10/20,'003', 15);
